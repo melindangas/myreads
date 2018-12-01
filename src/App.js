25d48f06cca057471e054002ead53e2fd7f73789
Melinda Alcantara <mely@Melindas-MBP.attlocal.net>
2018-12-01 07:37:19 -0800

    fixed react routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import './App.css'
-import {search} from './BooksAPI'
+import {search, get} from './BooksAPI'
 import Book from './Book'
 import Shelf from './Shelf'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 
 const bookShelves = [
   {id:"currentlyReading", name:"Currently Reading"}, 
@@ -11,13 +12,6 @@ const bookShelves = [
 ]
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     searchTerm: "",
     searchResults:[],
     books:[]
@@ -27,28 +21,51 @@ class BooksApp extends React.Component {
     super(props)
     this.changeSearch = this.changeSearch.bind(this)
     this.renderSearch = this.renderSearch.bind(this)
+    this.renderShelves = this.renderShelves.bind(this)
     this.searchBook = this.searchBook.bind(this)
     this.handleChangeShelf = this.handleChangeShelf.bind(this)
   }
+  
+  componentDidMount() {
+    this.loadFromStorage()
+  }
 
   handleChangeShelf(book, shelf) {
-    var allBooks = this.state.books   // copy state to modify
-    var bookFound = false
-    allBooks.forEach((item, index) => {
-      if (item.id === book.id){  // check already existing book
-        bookFound = true
-        if (shelf === "none") {
-          allBooks.splice(index, 1)  // delete from array if none was selected
-        } else {
-          item.shelf = shelf   // add shelf info
-        }
-      }
-    })
+    let bookFound = false
+    let newList;
+    if (shelf === 'none') {
+      newList = this.state.books.filter(item => item.id !== book.id) // only returns books that didnt change 
+    } else {
+      newList = this.state.books.slice().map(item => {
+        
+        if (item.id === book.id) { // check already existing book
+          item.shelf = shelf
+          bookFound = true      
+        } 
+        return item
+      })
+    }
     if (!bookFound) {     // if not found, adds book for the first time
       book.shelf = shelf
-      allBooks.push(book)
+      newList = this.state.books.concat(book)
     }
-    this.setState({books: allBooks}) // update state with changes
+
+    this.saveToStorage(newList)
+    this.setState({books: newList}) // update state with changes
+  }
+  
+  saveToStorage(books) {
+    localStorage.setItem('books', JSON.stringify(books.map(book => ({id: book.id, shelf: book.shelf}))))
+  }
+  
+  loadFromStorage() {
+    const books = JSON.parse(localStorage.getItem('books')) || []
+    books.forEach(book => {
+      get(book.id)
+      .then(result => {
+        this.handleChangeShelf(result, book.shelf)
+      })
+    })
   }
 
   changeSearch(e) {
@@ -84,7 +101,7 @@ class BooksApp extends React.Component {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link to="/" className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
             <input type="text" placeholder="Search by title or author" onChange={this.changeSearch} value={this.state.searchTerm}/>
           </div>
@@ -101,29 +118,36 @@ class BooksApp extends React.Component {
     )
   }
 
+  renderShelves() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            {bookShelves.map(shelf => {
+                let books = this.state.books.filter(book => book.shelf === shelf.id)  // filters books for the shelf
+                return <Shelf title={shelf.name} id={shelf.id} key={shelf.id} books={books} handleChangeShelf={this.handleChangeShelf} /> 
+              })
+            }
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
+        </div>
+      </div>
+    )
+  }
+
   render() {
     return (
-      <div className="app">
-        {this.state.showSearchPage ? this.renderSearch() : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                {bookShelves.map(shelf => {
-                    let books = this.state.books.filter(book => book.shelf === shelf.id)  // filters books for the shelf
-                    return <Shelf title={shelf.name} id={shelf.id} key={shelf.id} books={books} handleChangeShelf={this.handleChangeShelf} /> 
-                  })
-                }
-              </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
+        <BrowserRouter>
+          <div className="app">
+            <Route path="/" exact component={this.renderShelves}></Route>
+            <Route path="/search" exact component={this.renderSearch}></Route>
           </div>
-        )}
-      </div>
+        </BrowserRouter>
     )
   }
 }
